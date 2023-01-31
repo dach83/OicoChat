d6e34c2bd6ee4192c284e3b6b5a0381c4738cbf6
dach83 <dach83@mail.ru>
2023-01-31 19:50:59 +0500

    Remove mockk

--- app/src/test/java/com/github/dach83/oicochat/di/KoinModulesTest.kt
+++ app/src/test/java/com/github/dach83/oicochat/di/KoinModulesTest.kt
@@ -1,27 +1,16 @@
 package com.github.dach83.oicochat.di
 
-import android.content.Context
-import io.mockk.mockkClass
-import org.junit.Rule
 import org.junit.Test
 import org.koin.dsl.koinApplication
 import org.koin.test.check.checkModules
-import org.koin.test.mock.MockProviderRule
 
 class KoinModulesTest {
-
-    @get:Rule
-    val mockProvider = MockProviderRule.create { clazz ->
-        mockkClass(clazz)
-    }
 
     @Test
     fun `check koin modules in MainApplication`() {
         koinApplication {
             modules(appModule)
-            checkModules {
-                withInstance<Context>()
-            }
+            checkModules()
         }
     }
 }
