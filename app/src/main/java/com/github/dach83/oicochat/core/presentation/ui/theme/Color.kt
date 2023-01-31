dca850497166728b694fc47ec2a1e5b17ead06e8
dach83 <dach83@mail.ru>
2023-01-31 13:02:28 +0300

    Create packages structure

--- app/src/main/java/com/github/dach83/oicochat/core/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/github/dach83/oicochat/core/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.github.dach83.oicochat.ui.theme
+package com.github.dach83.oicochat.core.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/github/dach83/oicochat/core/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/github/dach83/oicochat/core/presentation/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.github.dach83.oicochat.ui.theme
+package com.github.dach83.oicochat.core.presentation.ui.theme
 
 import android.app.Activity
 import android.os.Build
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun OicoChatTheme(
+fun Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
